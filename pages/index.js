3e27980d052f0959d97cab13e447fecce5f5ed7b
MaElmoon39 <maria.tortelloditta@gmail.com>
2024-07-05 09:06:03 -0500

    primer gráfico, parcialmt bien

--- pages/index.js
+++ pages/index.js
@@ -5,87 +5,83 @@ const ctxMidgraphCenter = document.querySelector("#canvas__midgraph-center");
 const ctxBottmLeft = document.querySelector("#canvas__bottm-left");
 const ctxBottmCenter = document.querySelector("#canvas__bottm-center");
 
-document.querySelector('#loadDataButton').addEventListener('click', loadDataDf);
+//document.querySelector('#loadDataButton').addEventListener('click', loadDataDf);
 //document.querySelector('#loadDataButton').addEventListener('click', loadDataDf);
 //document.querySelector('#loadDataButton').addEventListener('click', loadDataDf);
 
 //df_clean json file
-function loadDataDf() {
-  fetch('json/df_clean.json')
-  .then(function(response){
-    if(response.ok == true){
-      return response.json();
-    }
-  })
-  .then((dataInfo) => {
-    const continentInfo = Object.keys(dataInfo.continent);
-    const totalInfo = dataInfo.map(row => row.total);
-    const doughnutConfig = {
-      type: 'doughnut',
-      data: {
-        labels: continentInfo,
-        datasets: [{
-          label: 'Continente',
-          data: totalInfo,
-          backgroundColor: [
-        '#EF6A32',
-        '#FFF6E9',
-        '#36A2EB',
-        '#017351',
-        '#FBBF45',
-        '#B1AFFF',
-        '#A12A5E',
-        '#03C383',
-        '#26294A',
-        '#AAD962',
-        '#FF1A68',
-        '#01545A',
-        '#ED0345',
-        '#710162',
-        '#110141'
+fetch('json/df_clean.json')
+.then(function(response){
+  if(response.ok === true){
+    return response.json();
+  }
+})
+.then(function(data){
+  loadData(data, 'bar');
+  console.log(data);
+})
+.catch((error) => console.error("Error al cargar datos:", error));
 
-      ],
-      borderColor: [
-        '#EF6A32',
-        '#FFF6E9',
-        '#36A2EB',
-        '#017351',
-        '#FBBF45',
-        '#B1AFFF',
-        '#A12A5E',
-        '#03C383',
-        '#26294A',
-        '#AAD962',
-        '#FF1A68',
-        '#01545A',
-        '#ED0345',
-        '#710162',
-        '#110141'
-      ],
-      borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          }},
-        scales: {
-          y: {
-            beginAtZero: true
-          }
+function loadData(dataJson, plotType) {
+  new Chart(ctxGraphRight, {
+    type: plotType,
+    data: {
+      labels: dataJson.map(row => row.month),
+      datasets: [{
+        label: 'Continente',
+        data: dataJson.map(row => row.total),
+        backgroundColor: [
+      '#EF6A32',
+      '#FFF6E9',
+      '#36A2EB',
+      '#017351',
+      '#FBBF45',
+      '#B1AFFF',
+      '#A12A5E',
+      '#03C383',
+      '#26294A',
+      '#AAD962',
+      '#FF1A68',
+      '#01545A',
+      '#ED0345',
+      '#710162',
+      '#110141'
+
+    ],
+    borderColor: [
+      '#EF6A32',
+      '#FFF6E9',
+      '#36A2EB',
+      '#017351',
+      '#FBBF45',
+      '#B1AFFF',
+      '#A12A5E',
+      '#03C383',
+      '#26294A',
+      '#AAD962',
+      '#FF1A68',
+      '#01545A',
+      '#ED0345',
+      '#710162',
+      '#110141'
+    ],
+    borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        }},
+      scales: {
+        y: {
+          beginAtZero: true
         }
       }
-    };
-
-    const promVentas = new Chart(ctxGraphCenter, doughnutConfig);
-    return promVentas;
-  }
-)
-  .catch((error) => console.error("Error al cargar datos:", error));
+    }
+  });
 }
-
 //rfm_table json file
 /* function loadDataRFM() {
   fetch('json/rfm_table.json')
